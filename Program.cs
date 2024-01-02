aac10db8c0f37c05c936654397bee636b53b8a50
adrianommiranda <adriano.miranda.ti@gmail.com>
2024-01-02 16:19:30 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using DesafioPOO.Models;
+
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("(81) 98408-8888", "N68ko", "88888888", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+
+Iphone iphone = new Iphone("(82) 9177-5132", "iPhone 15", "55555555", 256);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegran");
+
+
